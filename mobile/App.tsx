892399603adb45fad5c37d5d2be7b416ef025c81
Rohit Agrawal <rohit777agrawal@gmail.com>
2024-02-12 11:12:38 -0600

    Setup for React and React Native Done --> Can be used as template to start

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
@@ -76,8 +69,9 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+          <Text>Hello there!</Text>
+          {/* <Section title="Step One">
+            Edit <Text style={styles.highlight}>kkbkjhkjh</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
@@ -89,7 +83,7 @@ function App(): React.JSX.Element {
           <Section title="Learn More">
             Read the docs to discover what to do next:
           </Section>
-          <LearnMoreLinks />
+          <LearnMoreLinks /> */}
         </View>
       </ScrollView>
     </SafeAreaView>
